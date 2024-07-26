ce780cce74b22e43c4aceed53d94366c81a0c688
SirrBarnes <torressergio1357@gmail.com>
2024-07-26 13:46:55 -0400

    finished coding

--- index.js
+++ index.js
@@ -1,97 +1,71 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
 import fs from "fs";
+import colors from "colors";
+import generateMarkdown from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         name: "title",
-        message: "Please enter title of Project:",
+        message: colors.brightMagenta("Please enter title of Project:"),
         type: "input"
     },
-    {   
+    {
         name: "description",
-        message: "Please enter description of Project:",
+        message: colors.brightMagenta("Please enter description of Project:"),
         type: "input"
     },
-    {   
+    {
         name: "installation",
-        message: "Please enter any installation instructions for the Project:",
+        message: colors.brightMagenta("Please enter any installation instructions for the Project:"),
         type: "input"
     },
-    {   
+    {
         name: "usage",
-        message: "Please enter usage instructions for the Project:",
+        message: colors.brightMagenta("Please enter usage instructions for the Project:"),
         type: "input"
     },
-    {   
-        name: "contribution",
-        message: "Please enter any contribution guidelines for the Project:",
+    {
+        name: "credits",
+        message: colors.brightMagenta("Please enter any contribution guidelines for the Project:"),
         type: "input"
     },
-    {   
+    {
         name: "test",
-        message: "Please enter test instructions for the Project:",
+        message: colors.brightMagenta("Please enter test instructions for the Project:"),
         type: "input"
     },
     {
         name: "license",
-        message: "Please select license used in the Project:",
+        message: colors.brightMagenta("Please select license used in the Project:"),
         type: "list",
-        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unilicense"]
+        choices: ["GNU AGP License v3.0", "GNU GP License v3.0", "GNU LGP License v3.0", "Mozilla Public License 2.0", "Apache License v2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"]
     },
     {
         name: "userName",
-        message: "Please enter your GitHub User Name:",
+        message: colors.brightMagenta("Please enter your GitHub User Name:"),
         type: "input"
     },
     {
         name: "email",
-        message: "Please enter your GitHub email:",
+        message: colors.brightMagenta("Please enter your GitHub email:"),
         type: "input"
-}];
+    }];
 
 // TODO: Create a function to write README file
 const writeToFile = (data) => {
     const parseData = JSON.parse(data);
-    console.log(data);
-    // fs.writeFile('README.md', data, (err) => {
-    //     err ? console.error(err) : console.log("created");
-    // });
 
-    fs.appendFile('README.md', `# ${parseData.title}\n`, (err) =>
-    err ? console.error(err) : console.log("commited"));
-
-    fs.appendFile('README.md', `## ${parseData.description}\n`, (err) =>
-        err ? console.error(err) : console.log("commited"));
-
-    fs.appendFile('README.md', `## ${parseData.installation}\n`, (err) =>
-        err ? console.error(err) : console.log("commited"));
-
-    fs.appendFile('README.md', `## ${parseData.usage}\n`, (err) =>
-        err ? console.error(err) : console.log("commited"));
-
-    fs.appendFile('README.md', `## ${parseData.contribution}\n`, (err) =>
-        err ? console.error(err) : console.log("commited"));
-
-    fs.appendFile('README.md', `## ${parseData.test}\n`, (err) =>
-        err ? console.error(err) : console.log("commited"));
-
-    fs.appendFile('README.md', `## ${parseData.license}\n`, (err) =>
-        err ? console.error(err) : console.log("commited"));
-
-    fs.appendFile('README.md', `## ${parseData.username}\n`, (err) =>
-        err ? console.error(err) : console.log("commited"));
-
-    fs.appendFile('README.md', `## ${parseData.email}\n`, (err) =>
+    fs.appendFileSync('./generatedReadMe/README.md', generateMarkdown(parseData), (err) =>
         err ? console.error(err) : console.log("commited"));
 }
 
 // TODO: Create a function to initialize app
 const init = () => {
     inquirer.prompt(questions)
-    .then((answers) => writeToFile(JSON.stringify(answers)))
-    .catch((error) => console.log(error))
+        .then((answers) => writeToFile(JSON.stringify(answers)))
+        .catch((error) => console.log(error))
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,125 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseLink = renderLicenseLink(license);
+
+ switch(licenseLink) {
+    case "https://choosealicense.com/licenses/agpl-3.0/#":
+
+    return `[![License](https://img.shields.io/badge/License-GNU_AGPL-brightgreen)](${licenseLink})`;
+
+    case "https://choosealicense.com/licenses/gpl-3.0/":
+
+    return `[![License](https://img.shields.io/badge/License-GNU_GPL-brightgreen)](${licenseLink})`;
+
+
+    case "https://choosealicense.com/licenses/lgpl-3.0/":
+
+    return `[![License](https://img.shields.io/badge/License-GNU_LGPL-brightgreen)](${licenseLink})`;
+
+    case "https://choosealicense.com/licenses/mpl-2.0/":
+
+    return `[![License](https://img.shields.io/badge/License-MPL-brightgreen)](${licenseLink})`;
+
+    case "https://choosealicense.com/licenses/apache-2.0/":
+
+    return `[![License](https://img.shields.io/badge/License-Apache-brightgreen)](${licenseLink})`;
+
+    case "https://choosealicense.com/licenses/mit/":
+      
+    return `[![License](https://img.shields.io/badge/License-MIT-brightgreen)](${licenseLink})`;
+
+    case "https://choosealicense.com/licenses/bsl-1.0/":
+
+    return `[![License](https://img.shields.io/badge/License-BSL-brightgreen)](${licenseLink})`;
+
+    case "https://choosealicense.com/licenses/unlicense/":
+
+    return `[![License](https://img.shields.io/badge/License-Unlicense-brightgreen)](${licenseLink})`;
+
+    default:
+
+    return "";
+ }
+ 
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  switch(license) {
+    case "GNU AGP License v3.0":
+      return "https://choosealicense.com/licenses/agpl-3.0/#"
+
+    case "GNU GP License v3.0": 
+      return "https://choosealicense.com/licenses/gpl-3.0/"
+
+    case "GNU LGP License v3.0":
+      return "https://choosealicense.com/licenses/lgpl-3.0/"
+    
+    case "Mozilla Public License 2.0":
+      return "https://choosealicense.com/licenses/mpl-2.0/"
+    
+    case "Apache License v2.0":
+      return "https://choosealicense.com/licenses/apache-2.0/"
+    
+    case "MIT License":
+      return "https://choosealicense.com/licenses/mit/"
+    
+    case "Boost Software License 1.0":
+      return "https://choosealicense.com/licenses/bsl-1.0/"
+    
+    case "The Unilicense":
+      return "https://choosealicense.com/licenses/unlicense/"
+
+    default:
+      return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `## License
+  
+  This License is covered under the ${license}
+  `
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `${renderLicenseBadge(data.license)}  
+  # ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Credits
+  ${data.credits}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  Github Profile: https://github.com/${data.userName}\n
+  You can send any additonal questions to
+  Email: ${data.email}
 `;
 }
 
